1752770ac29ab4cbcc9c35de3bd018f91a32cf2a
Pablo Kiel Castellanos Vargas <31465703+pablokiel@users.noreply.github.com>
2017-10-26 22:56:21 -0500

    Tarea 16
    Tarea16

---
+++ Tarea 16/tarea 16.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+
+int main (int argc, char  *argv){
+	
+	int var1=5;
+	int var2=2;
+	int resultado=0;
+	
+	//igua a 
+	
+	resultado= var1 == var2;
+	printf("%d == %d=%d\n", var1,var2,resultado);
+	
+	//menor igual que
+	
+	resultado=var1 <= var2;
+	printf("%d <= %d=%d\n", var1,var2,resultado);
+	
+	//Mayor que
+	
+	resultado= var1 > var2;
+	printf("%d > %d=%d\n", var1,var2,resultado);
+	
+	//diferente de
+	
+	resultado= var1 != var2;
+	printf("%d != %d=%d\n", var1,var2,resultado);
+	
+	return 0;
+	
+}
+	
